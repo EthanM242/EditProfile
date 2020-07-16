e643c12a1c62144ec0cf6970a8c6bae23c3485e3
Ethan M <ethanmui21@gmail.com>
2020-07-16 15:33:50 -0400

    Initial commit

---
+++ EditProfile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EditProfile
+//
+//  Created by Ethan Mui on 7/11/20.
+//  Copyright © 2020 Ethan Mui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
